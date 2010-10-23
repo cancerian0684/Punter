7bb19805246a6a9559b393d9814b1e1f0191b2be
Munish Chandel <cancerian0684@gmail.com>
2010-10-23 19:33:47 +0000

    added keygen command.

--- keystore.cmd
+++ keystore.cmd
@@ -1,4 +1,4 @@
-"keytool -genkey -alias punter -keyalg RSA -validity 1365 -keystore punter.keystore" 
+"keytool -genkey -dname "CN=Munish Chandel, OU=Global Markets, O=Sapient Corp, L=Gurgaon, S=Haryana, C=IN" -alias punter -keyalg RSA -validity 1365 -keystore punter.keystore -keypass punter -storepass punter" 
 
 
 password is punter
--- punter.bat
+++
@@ -1,2 +0,0 @@
-java -Xms256M -Xmx256M -cp bin/.;lib/*; com.sapient.punter.utils.Launcher
-pause
