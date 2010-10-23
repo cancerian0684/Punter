c9414a3d6c446ad543d4fa3b1df774e5359f6ec4
Munish Chandel <cancerian0684@gmail.com>
2010-10-23 19:14:48 +0000

    updated testcases.

--- test/com/sapient/punter/test/StaticDaoFacadeTestCase.java
+++ test/com/sapient/punter/test/StaticDaoFacadeTestCase.java
@@ -1,6 +1,7 @@
 package com.sapient.punter.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.Date;
 import java.util.List;
@@ -11,9 +12,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.sapient.kb.jpa.StaticDaoFacade;
 import com.sapient.punter.jpa.ProcessData;
 import com.sapient.punter.jpa.ProcessHistory;
-import com.sapient.punter.jpa.StaticDaoFacade;
 import com.sapient.punter.jpa.TaskData;
 import com.sapient.punter.jpa.TaskHistory;
 
@@ -43,10 +44,10 @@ public class StaticDaoFacadeTestCase {
 		TaskData task=new TaskData();
 		task.setProcess(process);
 		task.setName("Test Task");
-		task=StaticDaoFacade.createTask(task);
+		task=StaticDaoFacade.getInstance().createTask(task);
 		System.out.println(task.getId());
-		StaticDaoFacade.removeTask(task);
-		StaticDaoFacade.listTask(task.getId());
+		StaticDaoFacade.getInstance().removeTask(task);
+		StaticDaoFacade.getInstance().listTask(task.getId());
 		}
 	}
 
@@ -58,7 +59,7 @@ public class StaticDaoFacadeTestCase {
 		ph.setName("Test-1");
 		ph.setStartTime(new Date());
 		ph.setProcess(proc);
-		StaticDaoFacade.createProcessHistory(ph);
+		StaticDaoFacade.getInstance().createProcessHistory(ph);
 		TaskData task=new TaskData();
 		task.setId(1602L);
 		for(int i=0;i<=10;i++){
@@ -68,21 +69,21 @@ public class StaticDaoFacadeTestCase {
 		th.setSequence(1);
 		th.setStatus(true);
 		th.setLogs("dummy logs");
-		StaticDaoFacade.createTaskHistory(th);
+		StaticDaoFacade.getInstance().createTaskHistory(th);
 		
 		}
 	}
 
 	@Test
 	public void testGetProcessHistoryForProcessId() throws Exception{
-		List<ProcessHistory> phl = StaticDaoFacade.getProcessHistoryListForProcessId(1603L);
+		List<ProcessHistory> phl = StaticDaoFacade.getInstance().getProcessHistoryListForProcessId(1603L);
 		for (ProcessHistory processHistory : phl) {
 			System.out.println(processHistory.getId());
 		}
 	}
 	@Test
 	public void testGetProcessTaskHistoryForProcessHistoryId() throws Exception{
-		List<ProcessHistory> phl = StaticDaoFacade.getProcessHistoryListForProcessId(1603L);
+		List<ProcessHistory> phl = StaticDaoFacade.getInstance().getProcessHistoryListForProcessId(1603L);
 		for (ProcessHistory processHistory : phl) {
 			System.out.println(processHistory.getId());
 		}
@@ -92,12 +93,12 @@ public class StaticDaoFacadeTestCase {
 	public void testListTask() throws Exception{
 		com.sapient.punter.jpa.ProcessData process=new com.sapient.punter.jpa.ProcessData();
 		process.setId(712L);
-		List<TaskData> t1 = StaticDaoFacade.getProcessTasks(712L);
+		List<TaskData> t1 = StaticDaoFacade.getInstance().getProcessTasks(712L);
 		TaskData task=new TaskData();
 		task.setProcess(process);
 		task.setName("Test Task");
-		task=StaticDaoFacade.createTask(task);
-		List<TaskData> t2 = StaticDaoFacade.getProcessTasks(712L);
+		task=StaticDaoFacade.getInstance().createTask(task);
+		List<TaskData> t2 = StaticDaoFacade.getInstance().getProcessTasks(712L);
 		assertEquals(1, (t2.size()-t1.size()));
 	}
 
@@ -105,15 +106,15 @@ public class StaticDaoFacadeTestCase {
 	public void testGetProcessList() throws Exception{
 		com.sapient.punter.jpa.ProcessData process=new com.sapient.punter.jpa.ProcessData();
 		process.setName("UBS-101");
-		List<ProcessData> pl1 = StaticDaoFacade.getProcessList();
-		StaticDaoFacade.createProcess(process);
-		List<ProcessData> pl2 = StaticDaoFacade.getProcessList();
+		List<ProcessData> pl1 = StaticDaoFacade.getInstance().getProcessList();
+		StaticDaoFacade.getInstance().createProcess(process);
+		List<ProcessData> pl2 = StaticDaoFacade.getInstance().getProcessList();
 		assertEquals(1, (pl2.size()-pl1.size()));
 	}
 
 	@Test
 	public void testGetProcess() throws Exception{
-		List<ProcessData> pl1 = StaticDaoFacade.getProcessList();
+		List<ProcessData> pl1 = StaticDaoFacade.getInstance().getProcessList();
 		for (ProcessData process : pl1) {
 			System.out.println(process.getId()+" -- "+process.getName());
 		}
