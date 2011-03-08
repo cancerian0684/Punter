2e581cfd266745573541480be5de2a57c3d2e206
Munish Chandel <cancerian0684@gmail.com>
2011-03-08 12:55:12 +0000

    Added colors to Punter Logs.

--- src/com/sapient/punter/utils/TextAreaFIFO.java
+++ src/com/sapient/punter/utils/TextAreaFIFO.java
@@ -1,11 +1,13 @@
 package com.sapient.punter.utils;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Date;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -22,23 +24,39 @@ import javax.swing.text.Element;
 public class TextAreaFIFO extends JTextArea implements DocumentListener,TextAreaFIFOMBean
 {
 	private int lineBufferSize=5000;
-	private boolean followTails=true;
+	private volatile boolean followTails=true;
 	private MouseClickListener listener;
+	private static Color [] colorArray={Color.BLUE,Color.MAGENTA,Color.RED,new Color(51,132,0),new Color(51,0,153),Color.darkGray};
+	private Color currentColor;
+	private static int colorSeq=0;
+	public int getColorSeq() {
+		int tmp=colorSeq;
+		colorSeq++;
+		if(colorSeq>4)
+			colorSeq=0;
+		return tmp;
+	}
 	public boolean isFollowTails() {
 		return followTails;
 	}
 	@Override
 	public void setDocument(Document doc) {
 		super.setDocument(doc);
+		currentColor=colorArray[getColorSeq()];
 		getDocument().addDocumentListener( this );
 		listener=new MouseClickListener();
 		addMouseListener(listener);
+		if(followTails)
+			setForeground(currentColor);
 	}
 	public TextAreaFIFO()
 	{
+		currentColor=colorArray[getColorSeq()];
 		getDocument().addDocumentListener( this );
 		listener=new MouseClickListener();
 		addMouseListener(listener);
+		if(followTails)
+			setForeground(currentColor);
 		/*MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
 		try {
 			mbs.registerMBean(this,new ObjectName("punter.log.mbean:type=Punter-LogTextArea"+Thread.currentThread().getId()));
@@ -62,27 +80,23 @@ public class TextAreaFIFO extends JTextArea implements DocumentListener,TextArea
 			public void run()
 			{
 				removeLines();
-				if(followTails)
-				setCaretPosition(getDocument().getLength() );
+				if(followTails){
+					setCaretPosition(getDocument().getLength());
+				}
 			}
 		});
 	}
- 
 	public void removeUpdate(DocumentEvent e) {}
 	public void changedUpdate(DocumentEvent e) {}
- 
 	public void removeLines()
 	{
 		Element root = getDocument().getDefaultRootElement();
- 
 		while (root.getElementCount() > lineBufferSize)
 		{
 			Element firstLine = root.getElement(0);
- 
 			try
 			{
 				getDocument().remove(0, firstLine.getEndOffset());
-				
 			}
 			catch(BadLocationException ble)
 			{
@@ -90,7 +104,6 @@ public class TextAreaFIFO extends JTextArea implements DocumentListener,TextArea
 			}
 		}
 	}
- 
 	public static void main(String[] args)
 	{
 		final TextAreaFIFO textArea = new TextAreaFIFO();
@@ -98,7 +111,7 @@ public class TextAreaFIFO extends JTextArea implements DocumentListener,TextArea
 		textArea.setColumns(40);
 		JScrollPane scrollPane = new JScrollPane( textArea );
  
-		final Timer timer = new Timer(10, new ActionListener()
+		final Timer timer = new Timer(200, new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
@@ -123,7 +136,6 @@ public class TextAreaFIFO extends JTextArea implements DocumentListener,TextArea
 				timer.stop();
 			}
 		});
- 
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		frame.getContentPane().add( start, BorderLayout.NORTH );
@@ -138,12 +150,14 @@ public class TextAreaFIFO extends JTextArea implements DocumentListener,TextArea
         /*    System.out.println("TitleMouseListener");
             System.out.println("is consumed: " + e.isConsumed() + ", click count: " + e.getClickCount() + ", right button: "
                                + (e.getButton() != MouseEvent.BUTTON3));
-        */    if(e.getClickCount()==1){
+        */    if(e.getClickCount()==2){
             	if(followTails){
             		followTails=false;
+            		setForeground(Color.black);
             	}
             	else{
             		followTails=true;
+            		setForeground(currentColor);
             	}
             }
         }
