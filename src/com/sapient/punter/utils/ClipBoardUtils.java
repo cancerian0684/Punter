4c389bc27c846a9785bab46daa0867db9a67e78b
Munish Chandel <cancerian0684@gmail.com>
2011-11-02 22:15:21 +0000

    ClipBoard Utility classes for Image Copy and Paste.

---
+++ src/com/sapient/punter/utils/ClipBoardUtils.java
@@ -0,0 +1,56 @@
+package com.sapient.punter.utils;
+
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+public class ClipBoardUtils {
+    public static Image getClipboard() {
+        Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
+        try {
+            if (t != null && t.isDataFlavorSupported(DataFlavor.imageFlavor)) {
+                Image text = (Image) t.getTransferData(DataFlavor.imageFlavor);
+                return text;
+            }
+        } catch (UnsupportedFlavorException e) {
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
+    // This method writes a image to the system clipboard.
+// otherwise it returns null.
+    public static void setClipboard(Image image) {
+        ImageSelection imgSel = new ImageSelection(image);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(imgSel, null);
+    }
+
+    // This class is used to hold an image while on the clipboard.
+    public static class ImageSelection implements Transferable {
+        private Image image;
+
+        public ImageSelection(Image image) {
+            this.image = image;
+        }
+
+        // Returns supported flavors
+        public DataFlavor[] getTransferDataFlavors() {
+            return new DataFlavor[]{DataFlavor.imageFlavor};
+        }
+
+        // Returns true if flavor is supported
+        public boolean isDataFlavorSupported(DataFlavor flavor) {
+            return DataFlavor.imageFlavor.equals(flavor);
+        }
+
+        // Returns image
+        public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+            if (!DataFlavor.imageFlavor.equals(flavor)) {
+                throw new UnsupportedFlavorException(flavor);
+            }
+            return image;
+        }
+    }
+}
