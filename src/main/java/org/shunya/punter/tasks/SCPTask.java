8f7e20b0abca0b23671d4a93eb4b90b66df67e3a
Munish Chandel <cancerian0684@gmail.com>
2015-09-02 01:40:52 +0000

    Support to download and upload multiple files through SCP

--- src/main/java/org/shunya/punter/tasks/SCPTask.java
+++ src/main/java/org/shunya/punter/tasks/SCPTask.java
@@ -9,193 +9,225 @@ import java.awt.*;
 import java.io.*;
 import java.util.logging.Level;
 
-@PunterTask(author="munishc",name="SCPTask",description="SCP File to remote machine.",documentation= "src/main/resources/docs/SCPTask.html")
+@PunterTask(author = "munishc", name = "SCPTask", description = "SCP File to remote machine.", documentation = "src/main/resources/docs/SCPTask.html")
 public class SCPTask extends Tasks {
-	@InputParam(required = true,description="Hostname of Unix machine")
-	private String hostname;
-	@InputParam(required = true)
-	private String username;
-	@InputParam(required = true)
-	private String password;
-	@InputParam(required = true,description="Source File to be copied")
-	private String sourceFile;
-	@InputParam(required = true,description="Target Dir/File name")
-	private String targetFile;
+    @InputParam(required = true, description = "Hostname of Unix machine")
+    private String hostname;
+    @InputParam(required = true, description = "SSH port number, default 22")
+    private int port;
+    @InputParam(required = true, description = "Absolute path for private key location")
+    private String privateKey;
+    @InputParam(required = true)
+    private String username;
+    @InputParam(required = true)
+    private String password;
+    @InputParam(required = true, description = "Source File to be copied")
+    private String sourceFile;
+    @InputParam(required = true, description = "Target Dir/File name")
+    private String targetFile;
 
-	@Override
-	public boolean run() {
-		boolean status=false;
-		try{
+    @Override
+    public boolean run() {
+        boolean status = false;
+        try {
 //		  LOGGER.get().log(Level.INFO, outName);
-		  FileInputStream fis=null;
-	      JSch jsch=new JSch();
+            FileInputStream fis = null;
+            JSch jsch = new JSch();
 
-	      //jsch.setKnownHosts("/home/foo/.ssh/known_hosts");
+            //jsch.setKnownHosts("/home/foo/.ssh/known_hosts");
+            if (privateKey != null && !privateKey.isEmpty())
+                jsch.addIdentity(privateKey);
 
-	      Session session=jsch.getSession(username, hostname, 22);
-	      session.setPassword(password);
+            if(port == 0)
+                port = 22;
 
-	      // username and password will be given via UserInfo interface.
-	      UserInfo ui=new MyUserInfo();
-	      session.setUserInfo(ui);
+            Session session = jsch.getSession(username, hostname, port);
 
-	      // session.setConfig("StrictHostKeyChecking", "no");
+            if (password != null && !password.isEmpty())
+                session.setPassword(password);
 
-	      session.connect();
+            // username and password will be given via UserInfo interface.
+            UserInfo ui = new MyUserInfo();
+            session.setUserInfo(ui);
+
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
 //	      session.connect(30000);   // making a connection with timeout.
-	      LOGGER.get().log(Level.INFO, "Connected to Shell.");
-	      
-	   // exec 'scp -t rfile' remotely
-	      String command="scp -p -t "+targetFile;
-	      Channel channel=session.openChannel("exec");
-	      ((ChannelExec)channel).setCommand(command);
+            LOGGER.get().log(Level.INFO, "Connected to Shell.");
 
-	      // get I/O streams for remote scp
-	      OutputStream out=channel.getOutputStream();
-	      InputStream in=channel.getInputStream();
+            String[] srcFiles = sourceFile.split("[\r\n|\n\r|\r|\n|;|,]");
+            String[] tgtFiles = targetFile.split("[\r\n|\n\r|\r|\n|;|,]");
 
-	      channel.connect();
+            for (int index = 0; index < srcFiles.length; index++) {
+                String srcFile = srcFiles[index];
+                String tgtFile = tgtFiles[index];
+                // exec 'scp -t rfile' remotely
+                String command = "scp -p -t " + tgtFile;
+                Channel channel = session.openChannel("exec");
+                ((ChannelExec) channel).setCommand(command);
 
-	      if(checkAck(in)!=0){
-	    	  LOGGER.get().log(Level.SEVERE, "UnKnown Error transmitting the file.");
-	    	  return false;
-	      }
-	      
-	   // send "C0644 filesize filename", where filename should not include '/'
-	      long filesize=(new File(sourceFile)).length();
-	      command="C0644 "+filesize+" ";
-	      if(sourceFile.lastIndexOf('/')>0){
-	        command+=sourceFile.substring(sourceFile.lastIndexOf('/')+1);
-	      }
-	      else{
-	        command+=sourceFile;
-	      }
-	      command+="\n";
-	      out.write(command.getBytes()); out.flush();
-	      if(checkAck(in)!=0){
-	    	  LOGGER.get().log(Level.SEVERE, "UnKnown Error transmitting the file.");
-	    	  return false;
-	      }
-	      // send a content of lfile
-	      fis=new FileInputStream(sourceFile);
-	      byte[] buf=new byte[1024];
-	      while(true){
-	        int len=fis.read(buf, 0, buf.length);
-	        if(len<=0) break;
-	        	out.write(buf, 0, len);
-	        	out.flush();
-	      }
-	      fis.close();
-	      fis=null;
-	      // send '\0'
-	      buf[0]=0; out.write(buf, 0, 1); out.flush();
-	      if(checkAck(in)!=0){
-	    	  LOGGER.get().log(Level.SEVERE, "UnKnown Error transmitting the file.");
-	    	  return false;
-	      }
-	      out.close();
+                // get I/O streams for remote scp
+                OutputStream out = channel.getOutputStream();
+                InputStream in = channel.getInputStream();
 
-	      channel.disconnect();
-	      session.disconnect();
-	      LOGGER.get().log(Level.INFO, "File sent successfully : "+targetFile);
-	      status=true;
-		}catch (Exception e) {
-			LOGGER.get().log(Level.SEVERE,"Error in SCP operation",e);
-			e.printStackTrace();
-		}
-		return status;
-	}
-	static int checkAck(InputStream in) throws IOException{
-	    int b=in.read();
-	    // b may be 0 for success,
-	    //          1 for error,
-	    //          2 for fatal error,
-	    //          -1
-	    if(b==0) return b;
-	    if(b==-1) return b;
+                channel.connect();
 
-	    if(b==1 || b==2){
-	      StringBuffer sb=new StringBuffer();
-	      int c;
-	      do {
-		c=in.read();
-		sb.append((char)c);
-	      }
-	      while(c!='\n');
-	      if(b==1){ // error
-		System.out.print(sb.toString());
-	      }
-	      if(b==2){ // fatal error
-		System.out.print(sb.toString());
-	      }
-	    }
-	    return b;
-	  }
-public static class MyUserInfo implements UserInfo, UIKeyboardInteractive{
-    public String getPassword(){ return passwd; }
-    public boolean promptYesNo(String str){
-    	return true;
-    	}
+                if (checkAck(in) != 0) {
+                    LOGGER.get().log(Level.SEVERE, "UnKnown Error transmitting the file.");
+                    return false;
+                }
 
-    String passwd;
-    JTextField passwordField=(JTextField)new JPasswordField("Dare10dream$",20);
+                // send "C0644 filesize filename", where filename should not include '/'
+                long filesize = (new File(srcFile)).length();
+                command = "C0644 " + filesize + " ";
+                if (srcFile.lastIndexOf('/') > 0) {
+                    command += srcFile.substring(srcFile.lastIndexOf('/') + 1);
+                } else {
+                    command += srcFile;
+                }
+                command += "\n";
+                out.write(command.getBytes());
+                out.flush();
+                if (checkAck(in) != 0) {
+                    LOGGER.get().log(Level.SEVERE, "UnKnown Error transmitting the file.");
+                    return false;
+                }
+                // send a content of lfile
+                fis = new FileInputStream(srcFile);
+                byte[] buf = new byte[1024];
+                while (true) {
+                    int len = fis.read(buf, 0, buf.length);
+                    if (len <= 0) break;
+                    out.write(buf, 0, len);
+                    out.flush();
+                }
+                fis.close();
+                fis = null;
+                // send '\0'
+                buf[0] = 0;
+                out.write(buf, 0, 1);
+                out.flush();
+                if (checkAck(in) != 0) {
+                    LOGGER.get().log(Level.SEVERE, "UnKnown Error transmitting the file.");
+                    return false;
+                }
+                out.close();
+                channel.disconnect();
+                LOGGER.get().log(Level.INFO, "File sent successfully : " + tgtFile);
+            }
 
-    public String getPassphrase(){ return null; }
-    public boolean promptPassphrase(String message){ return true; }
-    public boolean promptPassword(String message){
-      Object[] ob={passwordField}; 
-      passwd=passwordField.getText();
-      return true;
-    }
-    public void showMessage(String message){
-    }
-    final GridBagConstraints gbc = 
-      new GridBagConstraints(0,0,1,1,1,1,
-                             GridBagConstraints.NORTHWEST,
-                             GridBagConstraints.NONE,
-                             new Insets(0,0,0,0),0,0);
-    private Container panel;
-    public String[] promptKeyboardInteractive(String destination,
-                                              String name,
-                                              String instruction,
-                                              String[] prompt,
-                                              boolean[] echo){
-      panel = new JPanel();
-      panel.setLayout(new GridBagLayout());
-
-      gbc.weightx = 1.0;
-      gbc.gridwidth = GridBagConstraints.REMAINDER;
-      gbc.gridx = 0;
-      panel.add(new JLabel(instruction), gbc);
-      gbc.gridy++;
-
-      gbc.gridwidth = GridBagConstraints.RELATIVE;
-
-      JTextField[] texts=new JTextField[prompt.length];
-      for(int i=0; i<prompt.length; i++){
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.gridx = 0;
-        gbc.weightx = 1;
-        panel.add(new JLabel(prompt[i]),gbc);
-
-        gbc.gridx = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weighty = 1;
-        if(echo[i]){
-          texts[i]=new JTextField(20);
+            session.disconnect();
+            status = true;
+        } catch (Exception e) {
+            LOGGER.get().log(Level.SEVERE, "Error in SCP operation", e);
+            e.printStackTrace();
         }
-        else{
-          texts[i]=new JPasswordField(20);
-        }
-        panel.add(texts[i], gbc);
-        gbc.gridy++;
-      }
-
-        String[] response=new String[prompt.length];
-        for(int i=0; i<prompt.length; i++){
-          response[i]=texts[i].getText();
-        }
-	return response;
+        return status;
     }
-  }
+
+    static int checkAck(InputStream in) throws IOException {
+        int b = in.read();
+        // b may be 0 for success,
+        //          1 for error,
+        //          2 for fatal error,
+        //          -1
+        if (b == 0) return b;
+        if (b == -1) return b;
+
+        if (b == 1 || b == 2) {
+            StringBuffer sb = new StringBuffer();
+            int c;
+            do {
+                c = in.read();
+                sb.append((char) c);
+            }
+            while (c != '\n');
+            if (b == 1) { // error
+                System.out.print(sb.toString());
+            }
+            if (b == 2) { // fatal error
+                System.out.print(sb.toString());
+            }
+        }
+        return b;
+    }
+
+    public static class MyUserInfo implements UserInfo, UIKeyboardInteractive {
+        public String getPassword() {
+            return passwd;
+        }
+
+        public boolean promptYesNo(String str) {
+            return true;
+        }
+
+        String passwd;
+        JTextField passwordField = (JTextField) new JPasswordField("Dare10dream$", 20);
+
+        public String getPassphrase() {
+            return null;
+        }
+
+        public boolean promptPassphrase(String message) {
+            return true;
+        }
+
+        public boolean promptPassword(String message) {
+            Object[] ob = {passwordField};
+            passwd = passwordField.getText();
+            return true;
+        }
+
+        public void showMessage(String message) {
+        }
+
+        final GridBagConstraints gbc =
+                new GridBagConstraints(0, 0, 1, 1, 1, 1,
+                        GridBagConstraints.NORTHWEST,
+                        GridBagConstraints.NONE,
+                        new Insets(0, 0, 0, 0), 0, 0);
+        private Container panel;
+
+        public String[] promptKeyboardInteractive(String destination,
+                                                  String name,
+                                                  String instruction,
+                                                  String[] prompt,
+                                                  boolean[] echo) {
+            panel = new JPanel();
+            panel.setLayout(new GridBagLayout());
+
+            gbc.weightx = 1.0;
+            gbc.gridwidth = GridBagConstraints.REMAINDER;
+            gbc.gridx = 0;
+            panel.add(new JLabel(instruction), gbc);
+            gbc.gridy++;
+
+            gbc.gridwidth = GridBagConstraints.RELATIVE;
+
+            JTextField[] texts = new JTextField[prompt.length];
+            for (int i = 0; i < prompt.length; i++) {
+                gbc.fill = GridBagConstraints.NONE;
+                gbc.gridx = 0;
+                gbc.weightx = 1;
+                panel.add(new JLabel(prompt[i]), gbc);
+
+                gbc.gridx = 1;
+                gbc.fill = GridBagConstraints.HORIZONTAL;
+                gbc.weighty = 1;
+                if (echo[i]) {
+                    texts[i] = new JTextField(20);
+                } else {
+                    texts[i] = new JPasswordField(20);
+                }
+                panel.add(texts[i], gbc);
+                gbc.gridy++;
+            }
+
+            String[] response = new String[prompt.length];
+            for (int i = 0; i < prompt.length; i++) {
+                response[i] = texts[i].getText();
+            }
+            return response;
+        }
+    }
 }
