644fca5ee27429187966327bb5b911cd59424b3b
Munish Chandel <cancerian0684@gmail.com>
2017-12-18 08:29:06 +0530

    Allowed multiple unix commands to be run in single UnixCommandTask

--- src/main/java/org/shunya/punter/tasks/UnixCommandTask.java
+++ src/main/java/org/shunya/punter/tasks/UnixCommandTask.java
@@ -8,6 +8,7 @@ import org.shunya.punter.utils.StringUtils;
 import java.io.File;
 import java.io.InputStream;
 import java.lang.Process;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @PunterTask(author = "munishc", name = "UnixCommandTask", documentation = "src/main/resources/docs/SystemCommandTask.html")
@@ -21,36 +22,44 @@ public class UnixCommandTask extends Tasks {
     @InputParam(required = false, description = "Should wait for the Process termination or not ?")
     public boolean waitForTerminate = true;
 
+    StringBuffer commandOutputBuffer = new StringBuffer();
+    StringBuffer commandErrorBuffer = new StringBuffer();
+
     @Override
     public boolean run() {
         final AtomicBoolean status = new AtomicBoolean(true);
-        try {
-            String[] commands = command.split("[ ]");
-            ProcessBuilder builder = new ProcessBuilder(commands);
-            builder.directory(new File(workingDir));
-            builder.redirectErrorStream(true);
-            Process child = builder.start();
-
-            final Logger logger = LOGGER.get();
-            Thread captureProcessStreams = new Thread(() -> {
-                try {
-                    startOutputAndErrorReadThreads(child.getInputStream(), child.getErrorStream(), logger);
-                } catch (AssertionMessageException e) {
-                    status.set(false);
-                    logger.error(e.getMessage());
-                } catch (Exception e) {
-                    status.set(false);
-                    logger.error(StringUtils.getExceptionStackTrace(e));
+        getTaskHistory().setActivity("Connecting to Shell");
+        getObserver().update(getTaskHistory());
+        try(Scanner stk = new Scanner(command).useDelimiter("\r\n|\n\r|\r|\n")) {
+            while (stk.hasNext()) {
+                String singleCommand = stk.next().trim();
+                getTaskHistory().setActivity(singleCommand+" ...");
+                getObserver().update(getTaskHistory());
+                String[] commands = singleCommand.split("[ ]");
+                ProcessBuilder builder = new ProcessBuilder(commands);
+                builder.directory(new File(workingDir));
+                builder.redirectErrorStream(true);
+                Process child = builder.start();
+                final Logger logger = LOGGER.get();
+                Thread captureProcessStreams = new Thread(() -> {
+                    try {
+                        startOutputAndErrorReadThreads(child.getInputStream(), child.getErrorStream(), logger);
+                    } catch (Exception e) {
+                        status.set(false);
+                        logger.error(StringUtils.getExceptionStackTrace(e));
+                    }
+                });
+                captureProcessStreams.start();
+                captureProcessStreams.join();
+                if (waitForTerminate) {
+                    logger.info("Waiting for the process to terminate");
+                    child.waitFor();
                 }
-            });
-            captureProcessStreams.start();
-            captureProcessStreams.join();
-
-            if (waitForTerminate) {
-                logger.info("Waiting for the process to terminate");
-                child.waitFor();
             }
-
+            if (!assertMessage(commandOutputBuffer, commandErrorBuffer)) {
+                status.set(false);
+                LOGGER.get().error("output does not contain required message string - ["+successMessage+"]");
+            }
             getTaskHistory().setActivity("finished all tasks");
             getObserver().update(getTaskHistory());
         } catch (Exception e) {
@@ -61,16 +70,12 @@ public class UnixCommandTask extends Tasks {
     }
 
     private void startOutputAndErrorReadThreads(InputStream processOutputStream, InputStream processErrorStream, Logger logger) throws AssertionMessageException, Exception {
-        StringBuffer commandOutputBuffer = new StringBuffer();
         AsynchronousStreamReader asynchronousCommandOutputReaderThread = new AsynchronousStreamReader(processOutputStream, commandOutputBuffer, new MyLogDevice(logger), "OUTPUT");
         asynchronousCommandOutputReaderThread.start();
-        StringBuffer commandErrorBuffer = new StringBuffer();
         AsynchronousStreamReader asynchronousCommandErrorReaderThread = new AsynchronousStreamReader(processErrorStream, commandErrorBuffer, new MyLogDevice(logger), "ERROR");
         asynchronousCommandErrorReaderThread.start();
         asynchronousCommandOutputReaderThread.join();
         asynchronousCommandErrorReaderThread.join();
-        if (!assertMessage(commandOutputBuffer, commandErrorBuffer))
-            throw new AssertionMessageException("output does not contain required message string - ["+successMessage+"]");
     }
 
     private boolean assertMessage(StringBuffer commandOutputBuffer, StringBuffer commandErrorBuffer) {
